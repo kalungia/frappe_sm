c8f7e94893d84d96f06ffb73cd34f81a9b6f483e
Kalungi Abraham <abrahamskalungi@gmail.com>
2025-05-17 14:50:34 +0000

    add: fronted app

---
+++ frappe_sm/frappe_sm/doctype/social_settings/social_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Abraham Kalungi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Social Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
