6d65f0366e37ebce8953963b03945c2d61d70463
Abraham Kalungi <abrahamskalungi@gmail.com>
2025-05-20 09:06:13 +0200

    fix: update deps

--- social_media_management/vite.config.js
+++ social_media_management/vite.config.js
@@ -1,22 +1,46 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
 import frappeui from 'frappe-ui/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [frappeui(), vue()],
+  plugins: [
+    frappeui({
+      frappeProxy: true,
+      jinjaBootData: true,
+      buildConfig: {
+        indexHtmlPath: '../frappe_sm/www/social_media_management.html',
+        emptyOutDir: true,
+        sourcemap: true,
+      },
+    }), 
+    vue(),
+    vueJsx(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        display: 'standalone',
+        name: 'Frappe SMM',
+        short_name: 'Frappe SMM',
+        start_url: '/social_media_management',
+        description:
+          'Modern Social Media Management tool to supercharge your social media operations',
+        icons: [],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  build: {
-    outDir: `../${path.basename(path.resolve('..'))}/public/social_media_management`,
-    emptyOutDir: true,
-    target: 'es2015',
-  },
   optimizeDeps: {
-    include: ['frappe-ui > feather-icons', 'showdown', 'engine.io-client'],
+    include: [ 'feather-icons', 'showdown', 'tailwind.config.js', 'prosemirror-state', 'prosemirror-view', 'lowlight'],
   },
 })
