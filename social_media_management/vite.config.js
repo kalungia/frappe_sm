401289f793f42684e3cae7af8fe468a111e851b0
Abraham Kalungi <abrahamskalungi@gmail.com>
2025-05-21 16:06:18 +0200

    fix: upgrade tailwind

--- social_media_management/vite.config.js
+++ social_media_management/vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-//import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
@@ -13,7 +12,7 @@ export default defineConfig({
       frappeProxy: true,
       lucideIcons: true,
       jinjaBootData: true,
-    }), 
+    }),
     vue(),
     vueJsx(),
   ],
