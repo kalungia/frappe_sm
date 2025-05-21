58b92b32859d387a61584ba8f278dc728235ebe0
Abraham Kalungi <abrahamskalungi@gmail.com>
2025-05-21 08:59:10 +0200

    feat: add socket usage

--- social_media_management/src/main.js
+++ social_media_management/src/main.js
@@ -4,30 +4,69 @@ import "./assets/css/overrideTooltip.css"
 import "./assets/css/proseMirror.css"
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { initSocket } from './socket'
 import router from './router'
 import App from './App.vue'
 
 import { vTooltip } from 'floating-vue'
 import {
+  FrappeUI,
   Button,
-  Card,
   Input,
+  TextInput,
+  FormControl,
+  ErrorMessage,
+  Dialog,
+  Alert,
+  Badge,
   setConfig,
   frappeRequest,
-  resourcesPlugin,
+  FeatherIcon,
 } from 'frappe-ui'
 
-const app = createApp(App)
+let globalComponents = {
+  Button,
+  TextInput,
+  Input,
+  FormControl,
+  ErrorMessage,
+  Dialog,
+  Alert,
+  Badge,
+  FeatherIcon,
+}
+
+// create a pinia instance
+let pinia = createPinia()
+
+let app = createApp(App)
 
 setConfig('resourceFetcher', frappeRequest)
-
+app.use(FrappeUI)
+app.use(pinia)
 app.use(router)
-app.use(resourcesPlugin)
-
+app.use(translationPlugin)
+app.use(posthogPlugin)
 app.directive('tooltip', vTooltip)
+for (let key in globalComponents) {
+  app.component(key, globalComponents[key])
+}
 
-app.component('Button', Button)
-app.component('Card', Card)
-app.component('Input', Input)
-
-app.mount('#app')
+let socket
+if (import.meta.env.DEV) {
+  frappeRequest({ url: '/api/method/frappe_sm.www.frappe_sm.get_context_for_dev' }).then(
+    (values) => {
+      for (let key in values) {
+        window[key] = values[key]
+      }
+      socket = initSocket()
+      app.config.globalProperties.$socket = socket
+      app.mount('#app')
+    },
+  )
+} else {
+  socket = initSocket()
+  app.config.globalProperties.$socket = socket
+  app.mount('#app')
+}
